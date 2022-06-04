f12b09a7d92a8ba18e5cb82240b07384c385a777
Daniel Costa <danieldca01@gmail.com>
2022-06-04 16:23:21 -0300

    Avanço em JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade permitida é ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. Foi multado.`)
+}
+console.log(`Sempre use cinto de segurança`)
