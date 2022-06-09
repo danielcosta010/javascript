24e82498997ac8bf7a17b46dc3d09c08ce5dc0c3
Daniel Costa <danieldca01@gmail.com>
2022-06-09 12:44:49 -0300

    avanço JavaScript

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
+
+/*var c = 1
+while (c < 10) {
+    console.log(`Tudo Bem? ${c}`)
+    c++
+} Teste lógico feito no final*/
+
+var c = 1
+do {
+    console.log(`Olá ${c}`)
+        c++
+}while(c < 5) //Teste lógico feito no início
